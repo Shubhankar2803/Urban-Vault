85cd00e93eb59abea1eddca4f261fb065faa371b
Shubhankar2803 <singhshubhankar561@gmail.com>
2025-05-18 02:37:09 +0530

    shadcn components changed to match the theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <Button>Helloo</Button>
+   <div className="flex flex-col gap-y-4">
+    
+    <div><Button variant="elevated">hnji bete</Button></div>
+<div>
+  <Input placeholder="im a input"/>
+</div>
+<div>
+  <Progress value={50}/>
+</div>
    </div>
   );
 }
